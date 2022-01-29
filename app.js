a0b654d9c0e4e81ba6180b682a8786e89f6c5cd2
Martin <martin.ostrat@khk.ee>
2022-01-30 01:05:56 +0200

    added Add Task to list functionality

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ form.addEventListener('submit', addTask);
 
 function addTask(e) {
     const task = taskInput.value;
-    console.log(task);
+    const li = document.createElement('li');
+
+    li.className = 'collection-item';
+
+    const text = document.createTextNode(task);
+    li.appendChild(text);
+
+    const ul = document.querySelector('.collection');
+    ul.appendChild(li);
+
+    taskInput.value = '';
+
+    console.log(ul);
     e.preventDefault();
 }
