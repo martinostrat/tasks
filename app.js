c523b0460e1f8c3030669728372cd37b08c84da9
Martin <martin.ostrat@khk.ee>
2022-01-30 01:18:16 +0200

    Added delete task functionality

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const form = document.querySelector('form');
 const taskInput = document.querySelector('#task');
+const tasksList = document.querySelector('.collection');
 
 form.addEventListener('submit', addTask);
+tasksList.addEventListener('click', deleteTask);
+
+function deleteTask(e) {
+    if(e.target.textContent == 'X') {
+        if(confirm('Oled kindel, et soovid taski kustudada?')) {
+            e.target.parentElement.remove();
+        }
+    }
+}
 
 function addTask(e) {
     const task = taskInput.value;
@@ -11,6 +21,13 @@ function addTask(e) {
 
     const text = document.createTextNode(task);
     li.appendChild(text);
+
+    const link = document.createElement('a');
+    link.setAttribute('href', '#');
+    link.className = 'secondary-content';
+    link.appendChild(document.createTextNode('X'));
+
+    li.appendChild(link);
 
     const ul = document.querySelector('.collection');
     ul.appendChild(li);
