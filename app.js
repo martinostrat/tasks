b8f6cc9e3b8b0c4dce43ca2150efdcb98de47a3d
Martin <martin.ostrat@khk.ee>
2022-01-30 02:01:34 +0200

    Added load tasks from LS functionality

--- app.js
+++ app.js
@@ -6,10 +6,39 @@ const delTasksBtn = document.querySelector('#del-tasks')
 form.addEventListener('submit', addTask);
 tasksList.addEventListener('click', deleteTask);
 delTasksBtn.addEventListener('click', deleteTasks);
+document.addEventListener('DOMContentLoaded', getTasksFromLS);
+
+function getTasksFromLS() {
+    let tasks;
+
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach(function (tasksElement) {
+        const li = document.createElement('li');
+
+        li.className = 'collection-item';
+
+        const text = document.createTextNode(tasksElement);
+        li.appendChild(text);
+
+        const link = document.createElement('a');
+        link.setAttribute('href', '#');
+        link.className = 'secondary-content';
+        link.appendChild(document.createTextNode('X'));
+
+        li.appendChild(link);
+
+        const ul = document.querySelector('.collection');
+        ul.appendChild(li);
+    });
+}
 
 function deleteTask(e) {
-    if(e.target.textContent == 'X') {
-        if(confirm('Oled kindel, et soovid taski kustudada?')) {
+    if (e.target.textContent == 'X') {
+        if (confirm('Oled kindel, et soovid taski kustudada?')) {
             e.target.parentElement.remove();
             task = e.target.parentElement.firstChild.textContent;
             deleteTaskFromLS(task);
@@ -18,26 +47,26 @@ function deleteTask(e) {
 }
 
 function deleteTasks(e) {
-    if(e.target.textContent == 'Clear All Tasks') {
-        if(confirm('Oled kindel, et soovid kõik taskid kustudada?')) {
-            while(tasksList.firstChild) {
+    if (e.target.textContent == 'Clear All Tasks') {
+        if (confirm('Oled kindel, et soovid kõik taskid kustudada?')) {
+            while (tasksList.firstChild) {
                 tasksList.removeChild(tasksList.firstChild);
             }
             deleteAllTasksFromLS(task);
         }
     }
-} 
+}
 
 function deleteTaskFromLS(task) {
     let tasks;
 
-    if(localStorage.getItem('tasks') === null) {
+    if (localStorage.getItem('tasks') === null) {
         tasks = [];
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'))
     }
-    tasks.forEach(function(tasksElement, index) {
-        if(tasksElement === task) {
+    tasks.forEach(function (tasksElement, index) {
+        if (tasksElement === task) {
             tasks.splice(index, 1);
         }
     });
@@ -45,7 +74,7 @@ function deleteTaskFromLS(task) {
 }
 
 function deleteAllTasksFromLS() {
-    if(localStorage.getItem('tasks') === null) {
+    if (localStorage.getItem('tasks') === null) {
         let tasks = [];
         localStorage.setItem('tasks', JSON.stringify(tasks))
     }
@@ -71,7 +100,7 @@ function addTask(e) {
     const ul = document.querySelector('.collection');
     ul.appendChild(li);
 
-    
+
     saveTaskToLS(task);
 
     taskInput.value = '';
@@ -83,7 +112,7 @@ function addTask(e) {
 function saveTaskToLS(task) {
     let tasks;
 
-    if(localStorage.getItem('tasks') === null) {
+    if (localStorage.getItem('tasks') === null) {
         tasks = [];
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'))
